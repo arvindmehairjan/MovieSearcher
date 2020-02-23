8207a9ce062a0cfcd40b789e47196c9df5267747
despi <despi@LAPTOP-JL8GAFLU>
2020-02-23 16:49:18 +0100

    Added a bunch of new stuff

---
+++ assets/js/apiTransactions.js
@@ -0,0 +1,55 @@
+// Get values using the API
+const API_KEY = '21a87e7abc95923bf7812946fdaaa819';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=21a87e7abc95923bf7812946fdaaa819';
+
+function generateUrl(path) {
+    const url = `https://api.themoviedb.org/3${path}?api_key=21a87e7abc95923bf7812946fdaaa819`;
+    return url;
+}
+
+function requestMovies(url, onComplete, onError) {
+    fetch(url)
+    .then((res) => res.json()) 
+    .then(onComplete)
+    .catch(onError); 
+}
+
+function searchMovie(value) {
+    const path = '/search/movie';
+    const url = generateUrl(path) + '&query=' + value;
+
+    requestMovies(url, renderSearchMovies, handleError);
+}
+
+function getUpcomingMovies() {
+    const path = '/movie/upcoming';
+    const url = generateUrl(path);
+
+    const render = renderMovies.bind({ title: 'Upcoming Movies' })
+    requestMovies(url, render, handleError);
+}
+
+function getTopRatedMovies() {
+    const path = '/movie/top_rated';
+    const url = generateUrl(path);
+
+    const render = renderMovies.bind({ title: 'Top Rated Movies' })
+    requestMovies(url, render, handleError);
+}
+
+function getPopularMovies() {
+    const path = '/movie/popular';
+    const url = generateUrl(path);
+
+    const render = renderMovies.bind({ title: 'Popular Movies' })
+    requestMovies(url, render, handleError);
+}
+
+function nowPlayingMovies() {
+    const path = '/movie/now_playing';
+    const url = generateUrl(path);
+
+    const render = renderMovies.bind({ title: 'Movies Playing Now' })
+    requestMovies(url, render, handleError);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,139 @@
 // Selecting elements from the DOM
 const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
+const movieSearchable = document.querySelector('#movie-searchable');
+const movieContainer = document.querySelector('#movie-container');
+
+function handleError(error) {
+    console.log('Error: ', error)
+}
+
+function movieSection(movies) {
+    const section = document.createElement('section');
+    section.classList = 'section';
+
+    movies.map((movie) => {
+        if (movie.poster_path) {
+            const img = document.createElement('img');
+            img.src = IMAGE_URL + movie.poster_path;
+            img.setAttribute('data-movie-id', movie.id);
+
+            section.appendChild(img);
+        }
+    })
+
+    return section;
+}
+
+function createMovieContainer(movies, title = '') {
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const header = document.createElement('h2');
+    header.innerHTML = title;
+
+    const content = document.createElement('div');
+    content.classList = 'content';
+
+    const contentClose = `<p id="content-close">X</p>`;
+
+    content.innerHTML = contentClose;
+
+    const section = movieSection(movies);
+    
+    movieElement.appendChild(header);
+    movieElement.appendChild(section);
+    movieElement.appendChild(content);
+
+    return movieElement;
+}
+
+function renderSearchMovies(data) {
+            // data.results 
+            movieSearchable.innerHTML = '';
+            const movies = data.results;
+            const movieBlock = createMovieContainer(movies);
+            movieSearchable.appendChild(movieBlock);
+            console.log('Data: ', data)
+}
+
+function renderMovies(data) {
+    // data.results 
+    const movies = data.results;
+    const movieBlock = createMovieContainer(movies, this.title);
+    movieContainer.appendChild(movieBlock);
+    console.log('Data: ', data)
+}
 
 buttonElement.onclick = function(event) {
     event.preventDefault();
     const value = inputElement.value;
+    searchMovie(value);
+
+    inputElement.value = '';
     console.log("Value: ", value)
 }
+
+function createIframe(video) {
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube.com/embed/${video.key}`;
+    iframe.width = 360;
+    iframe.height = 315;
+    iframe.allowFullscreen = true;
+
+    return iframe;
+}
+
+function createVideoTemplate(data, content) {
+    content.innerHTML = '<p id="content-close">X</p>';
+    console.log('Videos: ', data)
+    const videos = data.results;
+    const length = videos.length > 4 ? 4 : videos.length;
+    const iframeContainer = document.createElement('div');
+
+    for (let i = 0; i < data.results.length; i++) {
+        const video = videos[i];
+        const iframe = createIframe(video);
+        iframeContainer.appendChild(iframe);
+        content.appendChild(iframeContainer);
+    }
+}
+
+// Event delegation
+document.onclick = function(event) {
+
+    const target = event.target;
+
+    if (target.tagName.toLowerCase() === 'img') {
+        const movieId = target.dataset.movieId;
+        console.log('Movie ID: ', movieId);
+        const section = event.target.parentElement;
+        const content = section.nextElementSibling;
+        content.classList.add('content-display');
+
+        const path = `/movie/${movieId}/videos`;
+        const url = generateUrl(path);
+        // Fetch movie videos
+        fetch(url)
+        .then((res) => res.json()) 
+        .then((data) => createVideoTemplate(data, content))
+        .catch((error) => {
+            console.log("Error: ", error)
+        });
+    }
+
+    if (target.id === 'content-close') {
+        const content = target.parentElement;
+        content.classList.remove('content-display');
+    }
+}
+
+searchMovie();
+
+getUpcomingMovies();
+
+getTopRatedMovies();
+
+getPopularMovies();
+
+nowPlayingMovies();
